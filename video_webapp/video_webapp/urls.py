c255afc882e19c0c8551db18c5b6d8db91cfef4c
Nicholas Durand <durandn@cs.washington.edu>
2019-02-21 11:58:36 -0800

    Added documentation

--- video_webapp/video_webapp/urls.py
+++ video_webapp/video_webapp/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
   path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
   path('password-reset-confrim/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
   path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-  path('posts', include('posts.urls')),\
+  path('posts', include('posts.urls')),
   path('', RedirectView.as_view(url='/posts'), name='home'),
 ]
 
